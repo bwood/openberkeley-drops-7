aaf009037c340a4f69d69d240f1b7c7de11d430f
Brian Wood <bwood@berkeley.edu>
2014-09-10 17:10:44 -0700

    Remove profiles/panopoly during pre-upgrade

--- profiles/openberkeley/scripts/beta4-upgrade/pre-upgrade.sh
+++ profiles/openberkeley/scripts/beta4-upgrade/pre-upgrade.sh
@@ -13,6 +13,9 @@ fi
 DRUSH=${DRUSH:-drush}
 DRUSH_OPTS=${DRUSH_OPTS:---strict=0}
 
+SITE_NAME=`echo $ALIAS | cut -d '.' -f2`
+SITE_UUID=`$DRUSH $DRUSH_OPTS psite-uuid $SITE_NAME | cut -d ':' -f 2`
+
 OLD_MODULES="panopoly_demo panopoly_admin panopoly_pages entity_view_mode better_formats conditional_styles views_slideshow_cycle views_slideshow ucb_openberkeley"
 
 for MODULE in $OLD_MODULES; do
@@ -23,3 +26,34 @@ for MODULE in $OLD_MODULES; do
   $DRUSH $DRUSH_OPTS $ALIAS pm-uninstall -y $MODULE
 done
 
+# change to sftp mode
+$DRUSH $DRUSH_OPTS psite-cmode $SITE_UUID dev sftp
+echo -n "Pausing to permit Pantheon server magic..."
+sleep 10
+
+# Remove profiles/panopoly. You can't do a recursive rm with SFTP, so we use a
+# PHP script piped into 'drush php-script' (scr).  Switching to SFTP mode
+# sets permissions so we can do the delete.
+echo 'function rrmdir($dir) {
+   if (is_dir($dir)) {
+     $objects = scandir($dir);
+     foreach ($objects as $object) {
+       if ($object != "." && $object != "..") {
+         if (filetype($dir."/".$object) == "dir") rrmdir($dir."/".$object); else unlink($dir."/".$object);
+       }
+     }
+     reset($objects);
+     rmdir($dir);
+   }
+}
+rrmdir("profiles/panopoly");' | $DRUSH $DRUSH_OPTS $ALIAS scr -
+
+echo -n "..."
+sleep 10
+# commit the changes resulting from the delete
+$DRUSH $DRUSH_OPTS psite-commit $SITE_UUID dev --message="Remove profiles/panopoly in preparation for upgrade."
+
+echo "..."
+sleep 10
+# change to git mode
+$DRUSH $DRUSH_OPTS psite-cmode $SITE_UUID dev git
